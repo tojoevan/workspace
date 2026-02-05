0634820edf9530c393da59b10d2d08ded7d2e9f1
Joevan <joevan@JoevandeMacBook-Air.local>
2026-02-06 00:57:51 +0800

    pin

--- reading_workbench/settings.py
+++ reading_workbench/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'http://127.0.0.1', 'http://localhost']
+
 
 # Application definition
 
